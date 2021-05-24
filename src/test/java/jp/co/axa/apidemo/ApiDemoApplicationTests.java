21f262bf0615a0abd26a1bb353558653b7603944
Yu Chu <cyjeff@gmail.com>
2021-05-25 00:17:16 +0900

    added unit testing for DELETE and PUT

--- src/test/java/jp/co/axa/apidemo/ApiDemoApplicationTests.java
+++ src/test/java/jp/co/axa/apidemo/ApiDemoApplicationTests.java
@@ -2,15 +2,17 @@ package jp.co.axa.apidemo;
 
 import org.junit.Test;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import org.springframework.http.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,10 +22,11 @@ import org.springframework.test.web.servlet.MockMvc;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ApiDemoApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void a_contextLoads() {
 	}
 
 	// setup mockMvc
@@ -45,28 +48,78 @@ public class ApiDemoApplicationTests {
 
 	// GET all
 	@Test
-	public void getAllEmployee() throws Exception {
+	public void b_getAllEmployee() throws Exception {
 		this.mockMvc.perform(get("/api/v1/employees"))
 		.andExpect(status().isOk())
-		.andExpect(content().json("[{'id': 1,'name': 'Jeff','salary': 100000,'department': 'IT'},{'id': 2,'name': 'Jack','salary': 120000,'department': 'HR'}]"));;
+		.andExpect(content().json("[{'id': 1,'name': 'Jeff','salary': 100000,'department': 'IT'},{'id': 2,'name': 'Jack','salary': 120000,'department': 'HR'}]"));
 	}
 
 	// GET single
 	@Test
-	public void getOneEmployee() throws Exception {
+	public void c_getOneEmployee() throws Exception {
 		this.mockMvc.perform(get("/api/v1/employees/1"))
 		.andExpect(status().isOk())
-		.andExpect(content().json("{'id': 1,'name': 'Jeff','salary': 100000,'department': 'IT'}"));;
+		.andExpect(content().json("{'id': 1,'name': 'Jeff','salary': 100000,'department': 'IT'}"));
 	}
 
 	// POST
-	@Test
-	public void postEmployee() throws Exception {
+	// add one employee and check that exist
+	@Test 
+	public void d_postEmployee() throws Exception {
 		this.mockMvc.perform(post("/api/v1/employees?name=Jay&salary=140000&department=Finance"))
-		.andExpect(status().isOk());
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString("Employee Saved Successfully")));;
 		this.mockMvc.perform(get("/api/v1/employees/3"))
 		.andExpect(status().isOk())
 		.andExpect(content().json("{'id': 3,'name': 'Jay','salary': 140000,'department': 'Finance'}"));
-		this.mockMvc.perform(delete("/api/v1/employees/3"));
+	}
+
+	// DELETE
+	// delete number 2 and check it does not exist
+	@Test 
+	public void e_deleteEmployee() throws Exception {
+		this.mockMvc.perform(delete("/api/v1/employees/2"))
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString("Employee Deleted Successfully")));
+		this.mockMvc.perform(get("/api/v1/employees"))
+		.andExpect(status().isOk())
+		.andExpect(content().json("[{'id': 1,'name': 'Jeff','salary': 100000,'department': 'IT'},{'id': 3,'name': 'Jay','salary': 140000,'department': 'Finance'}]"));
+	}
+
+	// delete and non-exstant employee and check the warning message
+	@Test
+	public void f_deleteEmployeeNotExist() throws Exception {
+		this.mockMvc.perform(delete("/api/v1/employees/4"))
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString("Requested User Does Not Exist")));
+		
+	}
+
+	// PUT
+	// modify one employee and verify the change
+	@Test
+	public void g_putEmployee() throws Exception {
+		this.mockMvc.perform(put("/api/v1/employees/1")
+		.contentType(MediaType.APPLICATION_JSON)
+		.content("{\"salary\":200000}"))
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString("Employee Updated Successfully")));
+		this.mockMvc.perform(get("/api/v1/employees/1"))
+		.andExpect(status().isOk())
+		.andExpect(content().json("{'id': 1,'name': 'Jeff','salary': 200000,'department': 'IT'}"));
+		
+	}
+
+	// modify an non-existant employee and check that a new one is added
+	@Test
+	public void g_putEmployeeNotExist() throws Exception {
+		this.mockMvc.perform(put("/api/v1/employees/4")		
+		.contentType(MediaType.APPLICATION_JSON)
+		.content("{\"salary\":200000}"))
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString("Employee Saved Successfully")));
+		this.mockMvc.perform(get("/api/v1/employees"))
+		.andExpect(status().isOk())
+		.andExpect(content().json("[{'id': 1,'name': 'Jeff','salary': 200000,'department': 'IT'},{'id': 3,'name': 'Jay','salary': 140000,'department': 'Finance'},{'id': 4,'name': null,'salary': 200000,'department': null}]"));
 	}
 }
